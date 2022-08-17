2cb258794014eb135a61d697a6fbe68627979851
Matheus <matheusbeaumord@gmail.com>
2022-08-16 21:05:04 -0300

    feat: frontend

--- src/services/index.js
+++ src/services/index.js
@@ -1,18 +1,5 @@
-import axios from 'axios';
-
 export default class FoodServices {
   static _withBaseUrl = (path) => {
     return `https://pwn-msba.herokuapp.com/api/${path}`;
   };
-
-  static login(e) {
-    console.log('🚀', e);
-
-    // return axios.post(FoodServices._withBaseUrl('seguranca/login'), {
-    //   body: {
-    //     login: userName,
-    //     senha: password,
-    //   },
-    // });
-  }
 }
